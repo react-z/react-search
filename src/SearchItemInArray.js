acfde474fbeb22574a281f61056a95bff4a51c11
C.J. Winslow <whoaa512@gmail.com>
2015-11-05 17:09:42 -0800

    use more efficient regex.text over match

--- src/SearchItemInArray.js
+++ src/SearchItemInArray.js
@@ -2,7 +2,7 @@ var SearchItemInArray = function (items, input) {
   var reg = new RegExp(input.split('').join('\\w*').replace(/\W/, ''), 'i')
 
   return items.filter(function (item) {
-    if (item.match(reg)) {
+    if (reg.test(item)) {
       return item
     }
   })
