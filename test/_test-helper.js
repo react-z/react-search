880a917f426c0426e8a23739e427359d38884440
C.J. Winslow <whoaa512@gmail.com>
2015-11-05 22:18:49 -0800

    add basic rendering test

---
+++ test/_test-helper.js
@@ -0,0 +1,13 @@
+var jsdom = require('jsdom')
+
+var doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+var win = doc.defaultView
+
+global.document = doc
+global.window = win
+
+Object.keys(window).forEach(function (key) {
+  if (!(key in global)) {
+    global[key] = window
+  }
+})
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,35 @@
+require('./_test-helper')
 import test from 'ava'
+import React from 'react'
+import ReactTestUtils from 'react-addons-test-utils'
+
+import Search from '../lib/search'
 import SearchItemInArray from '../src/SearchItemInArray'
 
+const {
+  findRenderedDOMComponentWithTag,
+  scryRenderedDOMComponentsWithTag,
+  renderIntoDocument
+} = ReactTestUtils
 const items = ['Steven', 'Sean', 'Stefan', 'Sam', 'Nathan']
 
 test('search items in array returns correctly', t => {
   t.is(SearchItemInArray(items, 'ste')[0], 'Steven')
+  t.end()
+})
+
+test('renders text input, autocomplete, and an empty list of items', t => {
+  const component = renderIntoDocument(
+    <Search
+        items={items}
+    />
+  )
+  const input = findRenderedDOMComponentWithTag(component, 'input')
+  const autocomplete = component.refs.autocomplete
+  const listOfItems = scryRenderedDOMComponentsWithTag(component, 'li')
+  t.ok(input)
+  t.ok(autocomplete)
+  t.ok(listOfItems)
+  t.ok(listOfItems.length === 0)
   t.end()
 })
