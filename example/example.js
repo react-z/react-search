d85cc864bf43f39add8b1dede911317407d348f9
StevenIseki <stevenisekimartin@gmail.com>
2016-09-23 16:36:23 +1000

    update tests

--- example/example.js
+++ example/example.js
@@ -1,4 +1,4 @@
-import Search from '../lib/Search' /* react-search */
+import Search from '../lib/Search' /* 'react-search' */
 import ReactDOM from 'react-dom'
 import React, { Component, PropTypes } from 'react'
 
